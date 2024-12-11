8de766b6921a7766bcc4d14cc2af2d4603d8bd77
Pooja Srikanth <121930344+Poojsri@users.noreply.github.com>
2024-12-11 08:45:46 +0530

    Add files via upload

---
+++ day1.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        unordered_map<int,int> mp;
+        for(auto i: nums){
+            mp[i]++;
+            if(mp[i] > 1){
+                return i;
+            }
+        }
+        return -1;
+        
+    }
+};
